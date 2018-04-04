bd6b16d5fc9adfdfb299fdbe00b4510ab91eb2cc
DavidMejia025 <davidndy@gmail.com>
2018-04-04 15:26:34 -0500

    Top applicant application tests

---
+++ spec/factories/top_applicant.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :top_applicant do
+     email      "joshua@one.com"
+     first_name "joshua"
+     last_name  "prpich"
+     country    "COL"
+     mobile     3224567756
+     status     0
+  end
+
+end
